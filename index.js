2cfbc79361acabdeff2f84b3ab0c90a231db4ad8
Winter-gr <aapa2222@naver.com>
2022-08-04 20:06:44 +0900

    add react folder

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/react', (req, res) => {
+  res.sendFile('/react/build/index.html') // __dirname 필요할 지도..?
 })
 
 app.listen(port, () => {
